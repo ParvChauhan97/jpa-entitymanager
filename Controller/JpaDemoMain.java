4ab97ed03079e61846b07f7ae392a81f94c5f546
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-17 15:23:57 +0530

    Implement user saving in JpaDemoMain
    Add user persistence logic in main method

--- Controller/JpaDemoMain.java
+++ Controller/JpaDemoMain.java
@@ -3,6 +3,13 @@ package Controller;
 public class JpaDemoMain {
     public static void main(String[] args) {
       EntityManager em = JPAUtil.getentitymanager();
-        
+
+        try {
+            UserClassHiber user = new UserClassHiber("rahul", "rahul@gmail.com");
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+            System.out.println("User saved: " + user.getId());
+        }
     }
 }
