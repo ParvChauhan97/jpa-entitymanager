72eec88e322e7709298cd8fd5eeb0b03545a6755
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-22 13:48:58 +0530

    Update in JpaDemoMain.java

--- Controller/JpaDemoMain.java
+++ Controller/JpaDemoMain.java
@@ -6,7 +6,7 @@ import jakarta.persistence.EntityManager;
 public class JpaDemoMain {
     public static void main(String[] args) {
       EntityManager em = JPAUtil.getentitymanager();
- 
+  
         try {
             UserClassHiber user = new UserClassHiber("rahul", "rahul@gmail.com");
             em.getTransaction().begin();
