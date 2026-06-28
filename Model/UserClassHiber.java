4f7d06658af03dd5c022bcc0f2ee8cfba1a5997b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-28 10:52:27 +0530

    Add JPA annotations to UserClassHiber
    Added JPA annotations for entity mapping and ID generation.

--- Model/UserClassHiber.java
+++ Model/UserClassHiber.java
@@ -1,3 +1,13 @@
+@Entity
 public class UserClassHiber {
   
+   @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+
+
+    UserClassHiber() {
+    }
 }
