2ab795bb380653bd3aa506b821491e27b33c6298
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-13 00:56:57 +0530

    Update in JPAUtil class

--- Service/JPAUtil.java
+++ Service/JPAUtil.java
@@ -4,10 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class JPAUtil { 
+public class JPAUtil {   
   
-  private static final EntityManagerFactory emf;
-
+  private static final EntityManagerFactory emf; 
   static {
         emf = Persistence.createEntityManagerFactory("MyJpaUtil");
     }
