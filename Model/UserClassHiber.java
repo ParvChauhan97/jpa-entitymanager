16d8d589ba198f9cf902653cfa550ef264f1fefc
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-19 23:48:33 +0530

    Update of name variable in UserClassHiber

--- Model/UserClassHiber.java
+++ Model/UserClassHiber.java
@@ -8,7 +8,7 @@ public class UserClassHiber {
    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String name;
+    private String name; 
     private String email;
 
 
