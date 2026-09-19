4d07df5f4a05870dbf24e9e4f94544b805cd2b3a
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-19 23:48:58 +0530

    Update of GeneratedValue annotation

--- Model/UserClassHiber.java
+++ Model/UserClassHiber.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class UserClassHiber {
   
    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)  
     private int id;
     private String name; 
     private String email;
